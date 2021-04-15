c328d06f13d1705c606d95c5efcc9728834bba23
Sangho Min <shmin@builton.co.kr>
2021-04-15 19:39:48 +0900

    [prod] import 된 상품 property를 설정함

--- src/suppliers/auction/AuctionProductImporter.ts
+++ src/suppliers/auction/AuctionProductImporter.ts
@@ -1,11 +1,14 @@
 import { App } from '../../app';
 import { Product } from '../../model/Product';
+import { Pricing } from '../../model/Pricing';
 
 export class AuctionProductImporter implements App.ProductImporter {
     constructor(private dataSource: App.AuctionProductSource) { }
 
     fetchProducts() {
         return this.dataSource.fetchProducts()
-            .map(p => new Product('AUCTION', null as any, null as any, null as any));
+            .map(p => new Product('AUCTION', p.id, p.title,
+                new Pricing(p.listPrice, p.listPrice - p.sellingPrice))
+            );
     }
 }
