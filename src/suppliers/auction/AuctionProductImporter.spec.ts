7c453650e1f599f716afc17bc4b682bbb303c128
Sangho Min <shmin@builton.co.kr>
2021-04-15 19:38:36 +0900

    [test] import 된 상품의 property 가 모두 설정되어 있어야 함

--- src/suppliers/auction/AuctionProductImporter.spec.ts
+++ src/suppliers/auction/AuctionProductImporter.spec.ts
@@ -1,6 +1,7 @@
 import { AuctionProductImporter } from "./AuctionProductImporter"
 import { auction } from "../../test/mock"
 import { AuctionProductSourceStub } from "../../test/stub/auction"
+import { Pricing } from "../../model/Pricing";
 
 describe('Auction Product Importer', () => {
     it('should have all Products', () => {
@@ -21,5 +22,18 @@ describe('Auction Product Importer', () => {
         actual.forEach(p => {
             expect(p.site).toBe('AUCTION');
         })
+    })
+
+    it('should set all properties', () => {
+        const source = auction.sources[0];
+        const stub = new AuctionProductSourceStub([source])
+        const sut = new AuctionProductImporter(stub)
+
+        const products = sut.fetchProducts();
+        const actual = products[0];
+
+        expect(actual.id).toBe(source.id);
+        expect(actual.name).toBe(source.title);
+        expect(actual.pricing).toEqual(new Pricing(source.listPrice, source.listPrice - source.sellingPrice));
     })
 })
