e4318ae4000e442e8f12a42919735edb8ced1589
Sangho Min <shmin@builton.co.kr>
2021-04-15 20:34:28 +0900

    [test] inventory 에 import 된 상품이 저장되어야 함

---
+++ src/synchronizer/ProductSynchronizer.spec.ts
@@ -0,0 +1,20 @@
+import { AuctionProductSourceStub } from "../test/stub/auction";
+import { AuctionProductImporter } from "../suppliers/auction/AuctionProductImporter";
+import { auction } from "../test/mock";
+import { ListPriceFilter } from "./ListPriceFilter";
+import { ProductSynchronizer } from "./ProductSynchronizer";
+
+describe('Product Synchronizer', () => {
+    it('should saves Products', () => {
+        const stub = new AuctionProductSourceStub(auction.sources)
+        const importer = new AuctionProductImporter(stub)
+        const validator = new ListPriceFilter(0)
+        const spy = new ProductInventorySpy();
+        const sut = new ProductSynchronizer(importer, validator, spy);
+
+        sut.run();
+
+        const expected = importer.fetchProducts();
+        expect(spy.getLog()).toEqual(expected);
+    })
+})
