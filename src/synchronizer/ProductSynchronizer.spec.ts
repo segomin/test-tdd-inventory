74822f3b038d8ba1d3c6d345d251eb2e0a032c0c
sangho <segoviammin@gmail.com>
2021-04-18 18:43:18 +0900

    [test] should not save into Inventory of invalid product

--- src/synchronizer/ProductSynchronizer.spec.ts
+++ src/synchronizer/ProductSynchronizer.spec.ts
@@ -18,4 +18,23 @@ describe('Product Synchronizer', () => {
         const expected = importer.fetchProducts();
         expect(spy.getLog()).toEqual(expected);
     })
+
+    it('should not save invalid product', () => {
+        // Arrange
+        const products = auction.sources;
+        const lowerBound = Math.max(...products.map(p => p.listPrice + 100));
+        const validator = new ListPriceFilter(lowerBound);
+
+        const stub = new AuctionProductSourceStub(products);
+        const importer = new AuctionProductImporter(stub);
+        const spy = new ProductInventorySpy();
+        const sut = new ProductSynchronizer(importer, validator, spy);
+
+        // Act
+        sut.run();
+
+        // Assert
+        expect(spy.getLog()).toEqual([]);
+    })
+
 })
